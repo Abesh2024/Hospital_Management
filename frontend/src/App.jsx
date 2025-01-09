291157c9a72e922c3dc28145e0b9659453c6c1f0
Abesh <abeshsarkar2030@gmail.com>
2025-01-09 21:44:26 +0530

    make protect route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,20 @@ import QueueManagement from './components/QueueManagement';
 import AppointmentManagement from './components/AppointmentManagement';
 import Login from './pages/Login';
 
-const App = () => (
-  <Router>
-    <div className="min-h-screen bg-gray-100">
-      <Routes>
-        <Route path="/" element={<QueueManagement />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/appointments" element={<AppointmentManagement />} />
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+     const token = localStorage.getItem("token");
+
+     return(
+       <Router>
+        <div className="min-h-screen bg-gray-100">
+          <Routes>
+            <Route path={token ? "/" : "/login"} element={<QueueManagement />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/appointments" element={<AppointmentManagement />} />
+          </Routes>
+        </div>
+      </Router>
+)
+};
 
 export default App;
